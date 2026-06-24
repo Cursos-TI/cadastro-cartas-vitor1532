be6c4786307b3b3fe31a074b3011d9646d3b8f71
Vitor <vitomartinsalmeida@gmail.com>
2026-06-23 22:02:59 -0300

    Primeiro Desafio - Nível Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,9 +6,65 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char firstCard[4], secondCard[4], firstState, secondState, firstCity[50], secondCity[50];
+  int firstPopulation, secondPopulation, firstTuristPoints, secondTuristPoints;
+  float firstArea, secondArea, firstPIB, secondPIB;
   // Área para entrada de dados
 
+  // Primeira carta
+  printf("PRIMEIRA CARTA - Uma letra de 'A' a 'H' (representando um dos oito estados): \n");
+
+  scanf(" %c", &firstState);
+
+  printf("PRIMEIRA CARTA - Código letra do estado seguida de um número de 01 a 04 (ex: A01, B03): \n");
+  scanf(" %s", firstCard);
+
+  getchar(); // remove o Enter do buffer
+
+  printf("PRIMEIRA CARTA - digite o nome da Cidade: \n");
+  fgets(firstCity, 50, stdin);
+
+  printf("PRIMEIRA CARTA - O número inteiro de habitantes da cidade (População):  \n");
+  scanf(" %d", &firstPopulation);
+
+  printf("PRIMEIRA CARTA - A área da cidade em quilômetros quadrados:  \n");
+  scanf(" %f", &firstArea);
+
+  printf("PRIMEIRA CARTA - O Produto Interno Bruto da cidade em bilhões de reais: \n");
+  scanf(" %f", &firstPIB);
+
+  printf("PRIMEIRA CARTA - A quantidade de pontos turísticos na cidade: \n");
+  scanf(" %d", &firstTuristPoints);
+
+  printf("CARTA CADASTRADA COM SUCESSO!!! Agora vamos à segunda carta... \n");
+
+
+  // Segunda carta
+  printf("SEGUNDA CARTA - Uma letra de 'A' a 'H' (representando um dos oito estados): \n");
+
+  scanf(" %c", &secondState);
+
+  printf("SEGUNDA CARTA - Código letra do estado seguida de um número de 01 a 04 (ex: A01, B03): \n");
+  scanf(" %s", secondCard);
+
+  getchar(); // remove o Enter do buffer
+
+  printf("SEGUNDA CARTA - digite o nome da Cidade: \n");
+  fgets(secondCity, 50, stdin);
+
+  printf("SEGUNDA CARTA - O número inteiro de habitantes da cidade (População):  \n");
+  scanf(" %d", &secondPopulation);
+
+  printf("SEGUNDA CARTA - A área da cidade em quilômetros quadrados:  \n");
+  scanf(" %f", &secondArea);
+
+  printf("SEGUNDA CARTA - O Produto Interno Bruto da cidade em bilhões de reais: \n");
+  scanf(" %f", &secondPIB);
+
+  printf("SEGUNDA CARTA - A quantidade de pontos turísticos na cidade: \n");
+  scanf(" %d", &secondTuristPoints);
+
+  printf("Parabéns! Suas cartas foram criadas com sucesso!\n Seguem as informações de cada carta:\n Carta 1:\n Estado: %c\n Código: %s\n Nome da Cidade: %s População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de reais\n Númnero de Pontos Turísticos: %d\n\n\n Carta 2:\n Estado: %c\n Código: %s\n Nome da Cidade: %s População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de reais\n Númnero de Pontos Turísticos: %d\n", firstState, firstCard, firstCity, firstPopulation, firstArea, firstPIB, firstTuristPoints, secondState, secondCard, secondCity, secondPopulation, secondArea, secondPIB, secondTuristPoints);
   // Área para exibição dos dados da cidade
 
 return 0;
